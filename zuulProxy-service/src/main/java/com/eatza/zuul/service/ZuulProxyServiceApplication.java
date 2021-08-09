0849d98a4a369f6abd6d9d9e55596032db929a10
nesh84 <nehapawas.sharma@gmail.com>
2021-08-09 07:03:26 +0000

    zuul and Eureka

---
+++ zuulProxy-service/src/main/java/com/eatza/zuul/service/ZuulProxyServiceApplication.java
@@ -0,0 +1,17 @@
+package com.eatza.zuul.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyServiceApplication.class, args);
+	}
+	
+	}
